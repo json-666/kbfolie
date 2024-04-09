45129c264ad6353257593648190dcd7e88605273
JBielawski <jakub@bielawski.me>
2024-04-09 20:59:34 +0200

    add poznaj naz blizej

---
+++ blocks/kbf-poznaj_nas_blizej/kbf-poznaj_nas_blizej.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Poznaj nas bliżej block template
+ *
+ * @package kbf
+ */
+
+// Support custom "anchor" values.
+$anchor = '';
+if (!empty($block['anchor'])) {
+	$anchor = 'id="' . esc_attr($block['anchor']) . '" ';
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$class_name = 'block__poznaj';
+
+if (!empty($block['className'])) {
+	$class_name .= ' ' . $block['className'];
+}
+
+$blockID =  substr($block['id'], 0, 12);
+
+// Load values and assign defaults.
+$fields = get_fields();
+?>
+<section class="<?php echo $class_name; ?>">
+	<div class="container">
+        <h2 class="fc-white fs-35 fw-600 text-center">Poznaj nas bliżej</h2>
+    </div>
+</section>
